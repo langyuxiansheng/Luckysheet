1d022a0c2ab0cdfde16d9162f43116646436a375
zhouliujun <1096432931@qq.com>
2021-09-29 17:52:10 +0800

    更新

--- src/global/api.js
+++ src/global/api.js
@@ -6760,12 +6760,13 @@ export function setPrintArea (type, direction, margins) {
 
     //标准的纸张才允许使用
     if (paper.includes(type)) {
-        const attr = getPaperSize(type, direction, margins);
-        const zoom = $('#luckysheet-zoom-ratioText').data('current-ratio')
+        const attr = getPaperSize(type, direction, margins,css);
+        const zoom = $('#luckysheet-zoom-ratioText').data('current-ratio');
         $('#luckysheet-print-area').css({
             display:'block',
             width: attr.width * zoom + 'px',
-            height: attr.height * zoom + 'px'
+            height: attr.height * zoom + 'px',
+            ...css
         })
         attr.zoom = zoom;
         // console.log(`标准的纸张才允许使用`, attr,zoom)
